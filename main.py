c032f7db3bbc8b8af5a8354b3af48a01025f628e
Arvin Singh <hgnis.nivra@hotmail.com>
2025-07-04 19:51:09 +0100

    add main extry point

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Sci-fi Technology Wikipedia Scraper
+Main entry point for the application
+"""
+
+from src.cli import run_scraper
+
+if __name__ == "__main__":
+    run_scraper()
